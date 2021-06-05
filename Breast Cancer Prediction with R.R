5304d42edcc4651eaaec25a81330769feabca898
nouraahmed8499 <nouraahmed404@gmail.com>
2021-06-06 00:18:40 +0200

    master

--- Breast Cancer Prediction with R.R
+++ Breast Cancer Prediction with R.R
@@ -1,10 +1,4 @@
----
-title: "Breast Cancer Prediction with R"
-output:
-  pdf_document: default
-  html_notebook: default
----
-```{r}
+#importing and exploring the data set
 
 data <- read.csv("breast cancer.csv",header=FALSE, sep=",")
 
@@ -12,34 +6,25 @@ str(data)
 head(data)
 summary(data)
 
-```
-
-```{r}
+#data pre processing 
 
 
 names(data) <- c('Id','Cl_thickness','Cell_size','Cell_shape','Marg_adhesion','Epith_c_size','Bare_nuclei','Bl_cromat','Normal_nucleoli','Mitoses','Class')
 data$Class[data$Class == 2] = 0
 data$Class[data$Class == 4] = 1
-```
-
-```{r}
+# replacing NA with 0 and then replacing 0 values with the mean (all of them in Bate_nuclei column)
 summary(data)
 sum(data == '?')
-```
-
-```{r}
+# Bate_nuclei range is 1-10 so it's safe to fill missing data with 0 and then replace 0 with mean
 data[data == '?'] <- 0
 data$Bare_nuclei <- as.numeric(data$Bare_nuclei)
 data$Bare_nuclei[data$Bare_nuclei == 0 ] <- mean(data$Bare_nuclei, na.rm =TRUE)
-```
-
-```{r}
+#The Id column is filtered out as it is not needed for designing the classifier
 data$Id <- NULL
 str(data)
 summary(data)
 
-```
-```{r}
+#splitting the data set into train and test sets
 
 library(caTools)
 set.seed(123)    
@@ -47,37 +32,31 @@ split=sample.split(data, SplitRatio = 0.8)
 training_set=subset(data,split==TRUE)
 test_set=subset(data,split==FALSE)
 dim(training_set) 
-```
 
-```{r}
+#feature scaling 
 
 
 training_set[,1:9] = scale(training_set[,1:9])
 test_set[,1:9] = scale(test_set[,1:9])
 
-```
-```{r}
+#logistic regression 
+
+#building the classifier 
 Classifier = glm(formula = Class ~ ., 
                  family = binomial, 
                  data = training_set)
-```
-
-```{r}
+#predicting the test set results
 prob_pred = predict(Classifier, type = 'response', newdata = test_set[1:9])
 prob_pred
 y_pred = ifelse(prob_pred > 0.5, 1, 0)
 y_pred
-```
-```{r}
+#confusion Matrix
 cm = table(test_set[,10], y_pred)
 cm
-```
-```{r}
+#calculating the accuracy without applying k-fold 
 accuracy = (cm[1,1] + cm[2,2]/(cm[1,1] + cm[2,2]+ cm[1,2] + cm[2,1]))
 accuracy
-```
-
-```{r}
+#applying k_fold Cross Validation
 library(caret)
 folds = createFolds(training_set$Class, k = 10)
 CrossValidation = lapply(folds, function(x){
@@ -95,16 +74,13 @@ CrossValidation = lapply(folds, function(x){
 CrossValidation
 accuracies = mean(as.numeric(CrossValidation))
 accuracies
-```
-
-```{r}
+#Grid search (Finding the perfect hyper parameters)
 library(caret)
 classifier = train(form = as.factor(Class) ~ ., data = training_set, method='glm')
 classifier
 classifier$bestTune
-```
 
-```{r}
+#Visualization
 
 summary(Classifier)
 summ = summary(Classifier)
@@ -114,7 +90,20 @@ imp_cols = names(p_values)
 imp_cols
 set = data
 plot(set)
-```
+# 2- Cell_size
+plot(Class ~ Cell_size, data = set, 
+     col = ifelse(set[, 10] == 1, 'green4', 'red3'), pch = 20, ylim = c(-0.2, 1),
+     main = "Logistic Regression for Classification")
+curve(predict(Classifier, data.frame(Cell_size = x), type = "response"), 
+      add = TRUE, lwd = 3, col = "dodgerblue")
+abline(v = -coef(Classifier)[1] / coef(Classifier)[2], lwd = 2)
 
+# 5- Epith_c_size
+plot(Class ~ Epith_c_size, data = set, 
+     col = ifelse(set[, 10] == 1, 'green4', 'red3'), pch = 20, ylim = c(-0.2, 1),
+     main = "Logistic Regression for Classification")
+curve(predict(Classifier, data.frame(Epith_c_size = x), type = "response"), 
+      add = TRUE, lwd = 3, col = "dodgerblue")
+abline(v = -coef(Classifier)[1] / coef(Classifier)[2], lwd = 2)
 
 
